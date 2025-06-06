ebb15025fa8ccda57e21b9222527a30b966e6f46
fbedesch <fbedesch@cern.ch>
2025-06-06 13:06:13 +0200

    First installation

---
+++ examples/LoadTrk.c
@@ -0,0 +1,14 @@
+#include <TROOT.h>
+
+void LoadTrk()
+{
+gROOT->Reset();
+//gROOT->ProcessLine(".L $PYTHIA8/lib/libpythia8.so");
+//gROOT->ProcessLine(".L libDelphes.so");
+gROOT->ProcessLine(".I ~/git/fbedesch/delphes2/delphes/examples/classes");
+gROOT->ProcessLine(".L examples/classes/SolGeom.cc+");
+gROOT->ProcessLine(".L examples/classes/TrkUtil.cc+");
+gROOT->ProcessLine(".L examples/classes/SolTrack.cc+");
+gROOT->ProcessLine(".L examples/classes/KalmanCk.cc+");
+gROOT->ProcessLine(".L examples/KalmanCheck.c+");
+}
